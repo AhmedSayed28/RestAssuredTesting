c059854d167ecc9bf8df86789e5d98017677d5df
Ahmed Ghaly <ahm.say14@gmail.com>
2024-12-08 20:58:46 +0200

    Create and implement the login api with static class User

--- src/test/java/com/qaCart/testCases/testInfo.java
+++ src/test/java/com/qaCart/testCases/testInfo.java
@@ -1,5 +1,6 @@
 package com.qaCart.testCases;
 
+import com.google.gson.Gson;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import org.testng.annotations.Test;
@@ -9,17 +10,29 @@ import javax.naming.ldap.HasControls;
 import java.util.HashMap;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class testInfo {
+
+    static class User{
+        private String email;
+        private String password;
+
+        public User(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
+    }
+
+
     @Test
     public void getCoursesInfo(){
+
+
 
         HashMap<String,String> infoHeaders = new HashMap<>();
         infoHeaders.put("type","WEB");
         infoHeaders.put("language","JAVA");
-
-
-
 
 //        Header typeHeader = new Header("type","WEB");
 //        Header langHeader = new Header("language","JAVA");
@@ -31,6 +44,23 @@ public class testInfo {
                 .headers(infoHeaders).log().all()
         .when().get("api/v1/info/courses")
         .then().log().ifError()
-                .assertThat().statusCode(200).log().body();
+                .assertThat().statusCode(200).
+                assertThat().body("count",equalTo(1));
+    }
+
+    @Test
+    public void loginTest(){
+        Gson gson = new Gson();
+        User user = new User("hatem@example.com","123456");
+
+        String reqBody = gson.toJson(user);
+
+        given()
+                .baseUri("https://todo.qacart.com/")
+                .header("Content-Type","application/json")
+                .body(reqBody)
+                .when()
+                .get("api/v1/students/login")
+                .then().log().all();
     }
 }
