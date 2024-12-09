5ccaa6b4143d98c0a805fe6b34dc81153b66fb80
Ahmed Ghaly <ahm.say14@gmail.com>
2024-12-10 01:24:58 +0200

    Import the dependancies of the reporter and init it

---
+++ src/test/java/com/utils/ExtentReportManager.java
@@ -0,0 +1,18 @@
+package com.utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.ITestListener;
+
+public class ExtentReportManager implements ITestListener {
+
+    public ExtentSparkReporter sparkReporter;
+    public ExtentReports extent;
+    public ExtentTest test;
+
+
+
+
+
+}
